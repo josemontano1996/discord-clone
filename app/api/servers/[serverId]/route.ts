8f315f64572925b94cedb948bdb656cc9f251a40
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-11-09 18:03:00 +0100

    added editing server settings

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,20 +1,15 @@
-import { NextResponse } from 'next/server';
-import { v4 as uuidv4 } from 'uuid';
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
 
 export async function PATCH(req: Request, { params }: { params: { serverId: string } }) {
   try {
     const profile = await currentProfile();
-
     if (!profile) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
 
-    if (!params.serverId) {
-      return new NextResponse('Server ID missing', { status: 400 });
-    }
-
+    const { name, imageUrl } = await req.json();
     const server = await db.server.update({
       where: {
         id: params.serverId,
@@ -22,13 +17,14 @@ export async function PATCH(req: Request, { params }: { params: { serverId: stri
         profileId: profile.id,
       },
       data: {
-        inviteCode: uuidv4(),
+        name,
+        imageUrl,
       },
     });
 
     return NextResponse.json(server);
   } catch (error) {
-    console.log('[SERVER_ID', error);
-    return new NextResponse('Internal error', { status: 500 });
+    console.log('SERVER_ID_PATCH', error);
+    return new NextResponse('Internal Error', { status: 500 });
   }
 }
